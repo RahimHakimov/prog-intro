cc8ca0184e0405732bdce58e434e2e1f4ecbf885
Rakhim Khakimov <rahimjon0777@gmail.com>
2020-11-19 17:59:44 +0500

    //I have deleted not working code

--- java-solutions/expressions/OpType.java
+++
@@ -1,9 +0,0 @@
-package expressions;
-
-/**
- * @author Rakhim Khakimov (ramhakimov@niuitmo.ru)
- */
-
-public enum OpType {
-    Add, Subtract, Multiply, Divide
-}
--- java-solutions/expressions/Type.java
+++
@@ -1,9 +0,0 @@
-package expressions;
-
-/**
- * @author Rakhim Khakimov (ramhakimov@niuitmo.ru)
- */
-
-public enum Type {
-    OPERATION, CONST, VARIABLE
-}
