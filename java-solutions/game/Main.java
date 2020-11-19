2982373e47de8ace34a37535ec85b22fc83d3711
Rakhim Khakimov <rahimjon0777@gmail.com>
2020-11-19 11:26:08 +0500

    //Example of Tournament on a board 5x5 and k equal to 4. We have 3 players(2 HumanPlayer and 1 RandomPlayer), and have Tournament will repeat twice (because c equal to 2)

---
+++ java-solutions/game/Main.java
@@ -0,0 +1,24 @@
+package game;
+
+import game.player.HumanPlayer;
+import game.player.Player;
+import game.player.RandomPlayer;
+import game.player.SequentialPlayer;
+
+import java.util.List;
+
+/**
+ * @author Rakhim Khakimov (ramhakimov@niuitmo.ru)
+ */
+public class Main {
+    public static void main(String[] args) {
+        int n = 5, m = 5, k = 4, c = 2, numberOfPlayers = 3;
+        Player[] players = new Player[] {new HumanPlayer(), new HumanPlayer(), new SequentialPlayer()};
+        final Tournament tournament = new Tournament(true, numberOfPlayers, players);
+        int []results = tournament.whoIsTheBestPlayer(m, n, k, c);
+        for (int i = 0; i< numberOfPlayers; ++i) {
+            System.out.println((i+1) + " "+results[i]);
+        }
+    }
+}
+
