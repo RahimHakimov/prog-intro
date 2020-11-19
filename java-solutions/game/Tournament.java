eb5f0200955402f6454fb21520f5b5437fc3cca4
Rakhim Khakimov <rahimjon0777@gmail.com>
2020-11-19 11:23:50 +0500

    //Add table showing of results

--- java-solutions/game/Tournament.java
+++ java-solutions/game/Tournament.java
@@ -2,13 +2,12 @@ package game;
 
 import game.player.Player;
 
-import java.util.List;
 import java.util.Random;
 
 /**
  * @author Rakhim Khakimov (ramhakimov@niuitmo.ru)
  */
-public class Tournament{
+public class Tournament {
     private final boolean log;
     private final Player[] players;
     private final int numberOfPlayers;
@@ -22,28 +21,49 @@ public class Tournament{
 
     public int[] whoIsTheBestPlayer(int m, int n, int k, int c) {
         results = new int[numberOfPlayers];
-        for (int t = 0; t<c; ++t) {
-            for (int i = 0; i< numberOfPlayers; ++i) {
-                for (int j = i+1; j<numberOfPlayers; ++j) {
+        int[][] TableOfResults = new int[numberOfPlayers][numberOfPlayers];
+        for (int t = 0; t < c; ++t) {
+            for (int i = 0; i < numberOfPlayers; ++i) {
+                for (int j = i + 1; j < numberOfPlayers; ++j) {
                     boolean whoIsFirst = new Random().nextBoolean();
-                    Game game = new Game(log, new Player[] {players[j], players[i]});
+                    Game game = new Game(log, new Player[]{players[j], players[i]});
                     if (whoIsFirst) {
-                        game = new Game(log, new Player[] {players[i], players[j]});
+                        game = new Game(log, new Player[]{players[i], players[j]});
                     }
                     int result = game.play(new MNKBoard(m, n, k));
                     if (result >= 0) {
                         if ((whoIsFirst && result == 0) || (!whoIsFirst && result == 1)) {
+                            TableOfResults[i][j] = 3;
+                            TableOfResults[j][i] = 0;
                             results[i] += 3;
                         } else {
+                            TableOfResults[i][j] = 0;
+                            TableOfResults[j][i] = 3;
                             results[j] += 3;
                         }
                     } else {
+                        TableOfResults[i][j] = 1;
+                        TableOfResults[j][i] = 1;
                         results[i] += 1;
                         results[j] += 1;
                     }
                 }
             }
+            if (log) {
+                System.out.print(" ");
+                for (int i = 0; i<numberOfPlayers; ++i) {
+                    System.out.print(" "+(i+1));
+                }System.out.println();
+                for (int i = 0; i<numberOfPlayers; ++i) {
+                    System.out.print((i+1)+" ");
+                    for (int j = 0; j<numberOfPlayers; ++j) {
+                        if (i!=j) System.out.print(TableOfResults[i][j]+" ");
+                        else System.out.print("# ");
+                    }System.out.println();
+                }
+            }
         }
         return results;
     }
+
 }
