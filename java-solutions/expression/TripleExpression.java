48eeb7697f34e3b65e1fecfe09ba38aa41ce43ea
Rakhim Khakimov <rahimjon0777@gmail.com>
2020-11-26 17:34:27 +0500

    //some little changes

--- java-solutions/expression/TripleExpression.java
+++
@@ -1,9 +0,0 @@
-package expression;
-
-/**
- * @author Rakhim Khakimov (ramhakimov@niuitmo.ru)
- */
-
-public interface TripleExpression extends ToMiniString {
-    int evaluate(int x, int y, int z);
-}
