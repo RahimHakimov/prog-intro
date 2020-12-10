730fc68ec4cd2097f1e40e333a73257c8810f4a1
Rakhim Khakimov <rahimjon0777@gmail.com>
2020-12-10 12:09:43 +0500

    //

--- java-solutions/expression/exceptions/ExpressionParser.java
+++ java-solutions/expression/exceptions/ExpressionParser.java
@@ -6,7 +6,7 @@ import expression.parser.StringSource;
  * @author Rakhim Khakimov (ramhakimov@niuitmo.ru)
  */
 
-public class ExpressionParser extends expression.parser.ExpressionParser {
+public class ExpressionParser extends expression.parser.ExpressionParser implements Parser {
     public ExpressionParser(StringSource stringSource) {
         super(stringSource);
     }
