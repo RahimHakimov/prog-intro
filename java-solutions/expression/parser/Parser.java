02cb4f1937c8c1dcdca284255742ea3a2126173c
Rakhim Khakimov <rahimjon0777@gmail.com>
2020-12-11 14:04:44 +0500

    //Parser from hw#11

--- java-solutions/expression/parser/Parser.java
+++ java-solutions/expression/parser/Parser.java
@@ -1,11 +1,10 @@
 package expression.parser;
 
 import expression.TripleExpression;
-import expression.exceptions.ParsingException;
 
 /**
  * @author Georgiy Korneev (kgeorgiy@kgeorgiy.info)
  */
 public interface Parser {
-    TripleExpression parse(String expression) throws ParsingException;
+    TripleExpression parse(String expression) throws Exception;
 }
